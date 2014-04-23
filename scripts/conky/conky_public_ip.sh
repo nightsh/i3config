12fd180a90a368d75ce0c114ef8af93c70e7e6a8
Victor Nițu <victor@debian.org.ro>
2014-04-23 13:27:19 +0300

    echoip.com -> curlmyip.com
    echoip.com died today.

--- scripts/conky/conky_public_ip.sh
+++ scripts/conky/conky_public_ip.sh
@@ -2,7 +2,7 @@
 
 wget -q --tries=10 --timeout=20 http://google.com
 if [[ $? -eq 0 ]]; then
-    echo $(curl -s http://echoip.com)
+    echo $(curl -s http://curlmyip.com)
 else
         echo "n/a"
 fi
